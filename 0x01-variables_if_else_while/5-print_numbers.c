c540666b0bf0d93b367c204d2e4d125f869e976a
hamzas7 <hamza.yasr7@gmail.com>
2023-10-09 13:39:35 -0700

    prints all single  digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing all sigle digits from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
