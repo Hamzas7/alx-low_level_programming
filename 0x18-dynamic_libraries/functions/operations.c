b32cd593fe2945959933d93c4fa29c5753ba1210
hamzas7 <hamza.yasr7@gmail.com>
2024-01-15 11:41:32 -0800

    libdynamic successfully createad

--- 0x18-dynamic_libraries/functions/operations.c
+++ 0x18-dynamic_libraries/functions/operations.c
@@ -53,11 +53,11 @@ int div(int i, int k)
 }
 
 /**
-  mod - finds the modulus of two integers
-  @i: first integer to add
-  @k: second integer to add
-  Return: the modulus
-*/
+ * mod - finds the modulus of two integers
+ * @i: first integer to add
+ * @k: second integer to add
+ * Return: the modulus
+ */
 
 int mod(int i, int k)
 {
